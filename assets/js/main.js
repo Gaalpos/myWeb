3087efa9a8fe7b3ff2409e3f3d32e56d2e879cec
Gaalpos <gabripazos13@gmail.com>
2024-01-22 20:43:28 +0100

    all

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,21 @@
 
 import { elementos } from "./elementos.js";
 
-const generarHTML = ({ name, partes, img }) => `
+const generarHTML = ({ name, items, img }) => `
     <div class="col col-sm-12 col-md-6 col-lg-4">
         <div class="card shadow-sm">
-            <img class="bd-placeholder-img card-img-top" width="1280px" height="auto"
-                xmlns="http://www.w3.org/2000/svg" role="img" preserveAspectRatio="xMidYMid slice"
+            <img class="bd-placeholder-img card-img-top" width="1280px" height="auto" style="border-radius: 15px;
+                xmlns="http://www.w3.org/2000/svg" role="img" preserveAspectRatio="xMidYMid slice" alt="${name}"
                 focusable="false" src="assets/imgs/${img}">
             <div class="card-body">
                 <p class="card-text">
-                    <h3>${name}</h3>
-                    <p>${partes}</p>
+                    <h3><strong>${name}</strong></h3>
+                    <p>${items}</p>
                 </p>
             </div>
         </div>
     </div>
 `;
 
-// Genera el HTML para cada elemento
 const html = elementos.map(generarHTML).join('');
-
-// Inserta el HTML en el contenedor
 document.querySelector('#contenedor').innerHTML = html;
